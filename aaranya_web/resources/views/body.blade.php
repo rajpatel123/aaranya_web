4b3df3a384bd9454b35a1949e5a07731ceb6b501
manishs <manishsj97@gmail.com>
2021-03-24 18:58:13 +0530

    new website code for aaranya

---
+++ aaranya_web/resources/views/body.blade.php
@@ -0,0 +1,571 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  <style>
+  </style>
+  <!-- this is link for any type of logo--> 
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <title>web</title>
+  </head>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    -->
+<style>
+
+.headmenu
+{
+  margin-left:350px;
+}
+.headright
+{
+  margin-left:400px;
+
+}
+.bodytable 
+{
+ margin-left:190px;
+ margin-top:60px;
+}
+.cardbody
+{
+  
+margin-left:200px;
+}
+
+.card  
+{
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  border: px solid #dddddd;
+  text-align: left;
+  padding: 15px;
+}
+.videobody
+{
+  margin-left:350px;
+ 
+}
+.feature
+{
+  background-color:lightgrey;
+}
+button
+{
+  margin-left:680px;
+}
+a {
+  text-decoration: none;
+  font-size: 22px;
+  color: black;
+
+}
+.bodyins
+{
+  background-color:lightgreen;
+
+}
+.appsection
+{
+  background-color:lightgreen;
+}
+.expsectiom
+{
+  background-color:lightblue;
+}
+.askquestion
+{
+  background-color:;
+  margin-left:180px;
+}
+.imglogo
+{
+  margin-left:280px;
+}
+.ques
+{
+  margin-left:200px;
+}
+.footersection
+{
+  background-color:;
+}
+</style>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    
+        <a class="navbar-brand" href="#">AARANYA</a>
+      
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+         </button>
+       
+    <div class="collapse navbar-collapse" id="navbarText">
+    <div class="headmenu">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Get App</a>
+        </li><li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+      </ul>
+      </div>
+      <div class="headright">
+      <div style="margin: 15px 0;">
+      <span class="navbar-text">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+      <a href="#"><i class="fa fa-facebook" fa-4x></i></a> 
+        </li>
+        <li class="nav-item">
+        <a href="#"> <i class="fab fa-instagram"></i></a>
+        </li>
+        <li class="nav-item">
+        <a href="#"><i class="fa fa-linkedin"></i></a>  
+        </li>
+        <li class="nav-item">
+        <a href="#"><i class="fa fa-twitter"></i></a>  
+        </li>
+      </ul>
+      </span>
+      </div>
+      </div>
+    </div>
+  </div>
+</nav>
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/svg/manishlogo.svg.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="/svg/manishlogo.svg.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="/svg/manishlogo.svg.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<table class="bodytable">
+<tr>
+
+<td>
+<h1>About  Aaranya</h1><p>Left aligned text on viewports sized SM (small) or wider.</p>
+<p>Left aligned text on viewports sized MD (medium) or wider.</p>
+<p>Left aligned text on viewports sized LG (large) or wider.</p>
+<p>Left aligned text on viewports sized XL (extra-large) or wider.</p>
+
+<td><img src="/svg/manishlogo.svg.jpg"   width="500" height='300' alt="..."></td>
+</td>
+</tr>
+</table>
+<!-- body card view section-->
+<!-- services  section start-->
+<br>
+<br>
+
+ <h1 class="text-center">Services</h1>
+<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+<p class="text-center">incididunt ut labore et dolore magna aliqua</p>
+<br>
+<br>
+<div class="text-center">
+<table class="cardbody">
+<tr>
+<td>
+  <div class="card" style="width: 16rem;">
+  <img class="card-img-top" src="/svg/manishlogo.svg.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h6 class="card-text">Computer related
+injuries
+</h6>
+   </div>
+</div>
+</td>
+<td>
+  <div class="card" style="width: 16rem;">
+  <img class="card-img-top" src="/svg/manishlogo.svg.jpg" alt="Card image cap">
+  <div class="card-body">
+  <h6 class="card-text">Women wellness </h6>
+   </div>
+</div>
+</td>
+<td>
+  <div class="card" style="width: 16rem;">
+  <img class="card-img-top" src="/svg/manishlogo.svg.jpg" alt="Card image cap">
+  <div class="card-body">
+  <h6 class="card-text">Skin & hair
+diseases</h6>
+    </div>
+</div>
+</td>
+<td>
+  <div class="card" style="width: 16rem;">
+  <img class="card-img-top" src="/svg/manishlogo.svg.jpg" alt="Card image cap">
+  <div class="card-body">
+  <h6 class="card-text">Stress reliever</h6>
+    </div>
+</div>
+</td>
+</tr>
+</table>
+<table class="cardbody">
+<tr>
+<td>
+  <div class="card" style="width: 16rem;">
+  <img class="card-img-top" src="/svg/manishlogo.svg.jpg" alt="Card image cap">
+  <div class="card-body">
+  <h6 class="card-text">Happy family</h6>
+    </div>
+</div>
+</td>
+<td>
+  <div class="card" style="width: 16rem;">
+  <img class="card-img-top" src="/svg/manishlogo.svg.jpg" alt="Card image cap">
+  <div class="card-body">
+  <h6 class="card-text">eight loss
+management</h6>
+     </div>
+</div>
+</td>
+<td>
+  <div class="card" style="width: 16rem;">
+  <img class="card-img-top" src="/svg/manishlogo.svg.jpg" alt="Card image cap">
+  <div class="card-body">
+<h6 class="card-text">Cholesterol BP Managt</h6>
+    </div>
+</div>
+</td>
+<td>
+  <div class="card" style="width: 16rem;">
+  <img class="card-img-top" src="/svg/manishlogo.svg.jpg" alt="Card image cap">
+  <div class="card-body">
+  <h6 class="card-text">Diabetes
+Management</h6>
+    </div>
+</div>
+</td>
+</tr>
+</table>
+<table class="cardbody">
+<tr>
+<td>
+  <div class="card" style="width: 16rem;">
+  <img class="card-img-top" src="/svg/manishlogo.svg.jpg" alt="Card image cap">
+  <div class="card-body">
+  <h6 class="card-text">Counselling & EAP</h6>
+   </div>
+</div>
+</td>
+<td>
+  <div class="card" style="width: 16rem;">
+  <img class="card-img-top" src="/svg/manishlogo.svg.jpg" alt="Card image cap">
+  <div class="card-body">
+  <h6 class="card-text">Lifestyle
+Diseases</h6>
+    </div>
+</div>
+</td>
+<td>
+  <div class="card" style="width: 16rem;">
+  <img class="card-img-top" src="/svg/manishlogo.svg.jpg" alt="Card image cap">
+  <div class="card-body">
+  <h6 class="card-text">Aaranya Yoga</h6>
+     </div>
+</div>
+</td>
+<td>
+  <div class="card" style="width: 16rem;">
+  <img class="card-img-top" src="/svg/manishlogo.svg.jpg" alt="Card image cap">
+  <div class="card-body">
+  <h6 class="card-text">Sleep Well</h6>
+   </div>
+</div>
+</td>
+</tr>
+</table>
+
+</div>
+<!-- services  section end-->
+<!-- why you need us section start-->
+
+<br>
+<br>
+<h1 class="text-center">Why You Need Us</h1>
+<br>
+<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+
+<br>
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/svg/manishlogo.svg.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="/svg/manishlogo.svg.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="/svg/manishlogo.svg.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+ 
+  </button>
+
+</div>
+<!-- why you need us section end-->
+<!-- featured video  section start-->
+<br>
+<br>
+<div class="feature">
+<br>
+<br>
+
+ <h1 class="text-center">Featured Video</h1>
+ <br>
+ <br>
+ <div class="videobody">
+ <video  controls width="800" height="500">
+
+    <source src="/svg/https___cdn.cnn.com_cnnnext_dam_assets_191120053137-03-milky-way-images-australia.svg.webm"
+            type="video/webm">
+
+    <source src="/svg/https___cdn.cnn.com_cnnnext_dam_assets_191120053137-03-milky-way-images-australia.svg.mp4"
+            type="video/mp4">
+
+    Sorry, your browser doesn't support embedded videos.
+  </video>
+  </div>
+  <br>
+<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+<p class="text-center">incididunt ut labore et dolore magna aliqua</p>
+<br>
+<br>
+</div>
+<!-- featured video  section end-->
+<!-- inspiring   section start-->
+<div class="bodyins">
+<br>
+<br>
+
+ <h1 class="text-center">Inspiring for your life</h1>
+ <br>
+ <br>
+<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+<p class="text-center">incididunt ut labore et dolore magna aliqua</p>
+<button type="button" class="btn btn-secondary">View more</button>
+<br>
+<br>
+</div>
+<!-- inspiring   section end-->
+<!-- meet our customer   section start-->
+<br>
+<br>
+
+ <h1 class="text-center">Meet Customer Satifaction</h1>
+ <br>
+ <br>
+<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+<p class="text-center">incididunt ut labore et dolore magna aliqua</p>
+<br>
+<br>
+<!-- meet our customer   section end-->
+<!-- download app section start-->
+<div class="appsection">
+<br>
+<br>
+
+ <h1 class="text-center">Download App</h1>
+ <br>
+ <br>
+<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+<p class="text-center">incididunt ut labore et dolore magna aliqua</p>
+<button type="button" class="btn btn-secondary">Playstore</button>
+<div class="phonelogo">
+
+<a href="#"><i class="fas fa-mobile-alt"></i> </a>
+
+</div>
+<br>
+<br>
+</div>
+<!-- download app section end-->
+<!-- Experencenced healer to save you best start-->
+
+<div class="expsectiom">
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+ <h1 class="text-center">Expericenced Healer To Serve You Best</h1>
+ <br>
+ <br>
+<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+<p class="text-center">incididunt ut labore et dolore magna aliqua</p>
+<br>
+<br>
+</div>
+<!-- Experencenced healer to save you best start-->
+
+<!--  Frequently Ask Question start  section-->
+
+
+<div class="askquestion">
+ <h1 class="text-center">Frequently Ask Question</h1>
+ <br>
+ <br>
+<p class="text-center">Ask your question and meet</p>
+<br>
+<br>
+
+<table>
+
+<tr>
+<td >
+<h3>
+Do you have any quesiton? Please
+</h3>
+<h3>
+ask here we ready to support
+</h3>
+<p>
+If your question is not list here, please feel</p>
+<p>free to make a manual support.</p>
+<h2>here us button</h2>
+</td>
+<div class="ques">
+<td>
+<div class="ques">
+<p>1.How much does it cost to be a credit card merchant?</p>
+  <p>2.How can I open a merchant account?</p>
+  <p>3.How much does it cost to be a credit card merchant?</p>
+  <p>4.How much does it cost to be a credit card merchant?</p>  
+  <p>5.How much does it cost to be a credit card merchant?
+</p>
+</div>
+  </td>
+  
+</tr>
+
+</table>
+<div>
+<!--  Frequently Ask Question end  section-->
+<br>
+<br>
+
+<table>
+<tr><td>
+<h1>Subscribe to our weekly news Subscribe letter
+</h1></td>
+<td>
+here button
+</td>
+</tr></table>
+<br>
+<br>
+<br>
+<!---footer section start-->
+<div class="footersection">
+<table><tr>
+<td>
+<h3>Contact  US </h3>
+<p>123 Mockup Street</p>
+<p>San Francisco, CA 10000
+</p>
+<p>(555) 555-555</p>
+<p>Copyright © Date Name. All rights reserved.</p>
+</td>
+<td>
+<div class="imglogo">
+<footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
+  <i class="fa fa-facebook-official w3-hover-opacity"></i>
+  <i class="fa fa-instagram w3-hover-opacity"></i>
+  <i class="fa fa-snapchat w3-hover-opacity"></i>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+  <p class="w3-medium">Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">Jpr technosoft</a></p>
+</footer>
+</div>
+</td>
+</tr>
+</table>
+</div>
+<!--footer section end-->
+</body>
+</html>
